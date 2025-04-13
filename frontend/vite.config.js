87f3865717da84b55e47b41b1fb8fd4f1e8af5b1
zahid shaikh <shaikhzahid178@gmail.com>
2025-04-13 22:44:25 +0530

    Your commit message describing the changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' 
+  }
 })
